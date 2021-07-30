b3d099e3d82dd802efd3b6de2ea87cd8c9002af6
Gilmar Moraes <gilmar14moraes@hotmail.com>
2021-07-30 10:50:04 -0300

    entrega da primera feature

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,7 @@ import 'package:flutter/material.dart';
 void main() =>
     runApp(MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100.0'),
-                subtitle: Text('1000'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('200.0'),
-                subtitle: Text('1000'),
-              ),
-            ),
-          ],
-        ),
+        body: ListaTransferencia(),
         appBar: AppBar(title: Text('Transferências'),),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
@@ -28,3 +11,43 @@ void main() =>
         ),
       ),
     ));
+
+class ListaTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(100.0, 1000)),
+        ItemTransferencia(Transferencia(200.0, 3002)),
+        ItemTransferencia(Transferencia(300.0, 3000)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
